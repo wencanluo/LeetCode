bb81657a68d0932ce0a79af9f9dbd31f37c54f5d
Wencan Luo <wencanluo.cn@gmail.com>
2013-08-05 14:07:32 -0400

    Add WildMatching

---
+++ CodeLeet/WildcardMatching.cpp
@@ -0,0 +1,79 @@
+/*Wildcard MatchingMar 16 '126416 / 25360
+Implement wildcard pattern matching with support for '?' and '*'.
+
+'?' Matches any single character.
+'*' Matches any sequence of characters (including the empty sequence).
+
+The matching should cover the entire input string (not partial).
+
+The function prototype should be:
+bool isMatch(const char *s, const char *p)
+
+Some examples:
+isMatch("aa","a") ? false
+isMatch("aa","aa") ? true
+isMatch("aaa","aa") ? false
+isMatch("aa", "*") ? true
+isMatch("aa", "a*") ? true
+isMatch("ab", "?*") ? true
+isMatch("aab", "c*a*b") ? false
+
+Author: Wencan Luo
+Language: C++
+Date: 08/04/2013
+*/
+
+#include<iostream>
+using namespace std;
+#include<climits>
+
+#include<vector>
+#include<string>
+#include<algorithm>
+
+#include<map>
+//#include<unordered_set>
+
+#include<queue>
+#include<set>
+
+// Definition for binary tree
+ struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+ TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+//Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+// Definition for an interval.
+struct Interval {
+    int start;
+    int end;
+    Interval() : start(0), end(0) {}
+    Interval(int s, int e) : start(s), end(e) {}
+};
+
+class Solution {
+public:
+    bool isMatch(const char *s, const char *p) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        
+    }
+};
+
+void main(){
+	Solution s;
+
+    int A[] = {2,3,1,1,4};
+    cout << s.jump(A, sizeof(A)/sizeof(A[0]));
+    
+	system("pause");
+}

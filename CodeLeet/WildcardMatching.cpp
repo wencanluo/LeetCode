66c16fcd5b5add317aa894afa4f2083c711c63ea
Wencan Luo <wencanluo.cn@gmail.com>
2013-08-05 20:23:44 -0400

    Add wildcard

--- CodeLeet/WildcardMatching.cpp
+++ CodeLeet/WildcardMatching.cpp
@@ -62,18 +62,154 @@ struct Interval {
 
 class Solution {
 public:
-    bool isMatch(const char *s, const char *p) {
+    bool isMatchRec(const char *s, int m, const char *p, int n){
+        if(m==0 && n==0) return true;
+        if(m==0){
+            for(int i=0;i<n;i++){
+                if(p[i] != '*') return false;
+            }
+            return true;  
+        }
+        if(n==0){
+            for(int i=0;i<m;i++){
+                if(s[i] != '*') return false;
+            }
+            return true;
+        } 
+        
+        if(s[m-1] == '*'){
+            if(p[n-1] == '*'){
+                return isMatchRec(s, m-1, p, n-1) || isMatchRec(s, m, p, n-1) || isMatchRec(s, m-1, p, n);
+            }else if(p[n-1] == '?'){
+                return isMatchRec(s, m-1, p, n-1) || isMatchRec(s, m, p, n-1);
+            }else{
+                return isMatchRec(s, m-1, p, n-1) || isMatchRec(s, m, p, n-1);
+            }
+        }else if(s[m-1] == '?'){
+            if(p[n-1] == '*'){
+                return isMatchRec(s, m-1, p, n-1) || isMatchRec(s, m, p, n-1) || isMatchRec(s, m-1, p, n);
+            }else if(p[n-1] == '?'){
+                return isMatchRec(s, m-1, p, n-1);
+            }else{    			
+                return isMatchRec(s, m-1, p, n-1);
+            }
+        }else{//letter
+            if(p[n-1] == '*'){
+                 return isMatchRec(s, m-1, p, n-1) || isMatchRec(s, m, p, n-1) || isMatchRec(s, m-1, p, n);
+            }else if(p[n-1] == '?'){
+                return isMatchRec(s, m-1, p, n-1);
+            }else{
+				if(s[m-1] == p[n-1]) return isMatchRec(s, m-1, p, n-1);
+				return false;
+            }
+        }
+    }
+    
+	bool isMatch(const char *s, const char *p) {//O(m+n)
+	}
+
+    bool isMatch_Omn(const char *s, const char *p) {//O(m*n) Time, O(n) Space
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
+        int m = strlen(s);
+        int n = strlen(p);
         
+		vector<bool>  A;
+		vector<bool>  B;
+		vector<bool>  C;
+		A.resize(n+1, false);
+		B.resize(n+1, false);
+		C.resize(m+1, false);
+
+		A[0] = true;
+		for(int j=0;j<n;j++){
+			if(p[j] != '*') break;
+			A[j+1] = true;
+        }
+
+        for(int i=0;i<m;i++){
+            if(s[i] != '*') break;
+			C[i+1] = true;
+        }
+   
+		for(int i=0;i<m;i++){
+			B[0] = C[i];
+
+			for(int j=0;j<n;j++){
+				B[j+1] = false;
+				if(s[i] == '*'){
+					if(p[j] == '*'){
+						B[j+1] = A[j] || B[j] || A[j+1];
+					}else{
+						B[j+1] = A[j] || B[j];
+					}
+				}else{
+					if(p[j] == '*'){
+						B[j+1] = A[j] || B[j] || A[j+1];
+					}else if(s[i] == '?' || p[j] == '?' || s[i] == p[j]){
+						B[j+1] = A[j];
+					}
+				}
+			}
+			A = B;
+		}
+        
+        return A[n];
+    }
+
+	bool isMatch_O2Space(const char *s, const char *p) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        int m = strlen(s);
+        int n = strlen(p);
+        
+		vector< vector<bool> > A;
+		A.resize(m+1);
+
+		for(int i=0;i<A.size();i++){
+			A[i].resize(n+1,false);
+		}
+
+		A[0][0] = true;
+		for(int i=0;i<m;i++){
+			if(s[i] != '*') break;
+			A[i+1][0] = true;
+        }
+
+        for(int j=0;j<n;j++){
+            if(p[j] != '*') break;
+			A[0][j+1] = true;
+        }
+   
+		for(int i=0;i<m;i++){
+			for(int j=0;j<n;j++){
+				if(s[i] == '*'){
+					if(p[j] == '*'){
+						A[i+1][j+1] = A[i][j] || A[i+1][j] || A[i][j+1];
+					}else{
+						A[i+1][j+1] = A[i][j] || A[i+1][j];
+					}
+				}else{
+					if(p[j] == '*'){
+						A[i+1][j+1] = A[i][j] || A[i+1][j] || A[i][j+1];
+					}else if(s[i] == '?' || p[j] == '?' || s[i] == p[j]){
+						A[i+1][j+1] = A[i][j];
+					}
+				}
+			}
+		}
+        
+        return A[m][n];
     }
 };
 
 void main(){
 	Solution s;
 
-    int A[] = {2,3,1,1,4};
-    cout << s.jump(A, sizeof(A)/sizeof(A[0]));
+	cout << s.isMatch("","*") << endl;
+	cout << s.isMatch("a","b") << endl;
+	cout << s.isMatch("a*","*a") << endl;
+	cout << s.isMatch("***","*") << endl;
     
 	system("pause");
 }

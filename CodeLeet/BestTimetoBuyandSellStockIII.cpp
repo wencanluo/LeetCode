d30657c30639cfc91713d60de9c9190045082f3a
Wencan Luo <wencanluo.cn@gmail.com>
2013-08-21 01:19:34 -0700

    Solved BestTimetoBuyandSellStockIII

--- CodeLeet/BestTimetoBuyandSellStockIII.cpp
+++ CodeLeet/BestTimetoBuyandSellStockIII.cpp
@@ -26,27 +26,95 @@ using namespace std;
 
 class Solution {
 public:
-    int maxProfit(vector<int> &prices) {
+	vector<int> maxProfitLeft(vector<int> &prices) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
-        int maxgain;
-		if(prices.size() == 0) return 0;
+		vector<int> r;
 
+		int n=prices.size();
+        int low, high, maxgain;
+		if(n == 0) return r;
+		r.resize(n);
+
+		low=high=prices[0];
 		maxgain = 0;
+		r[0] = 0;
 
-		for(int i=0;i<prices.size()-1;i++){
-			if(prices[i+1] > prices[i]){
-				maxgain = maxgain + prices[i+1] - prices[i];
+		for(int i=1;i<prices.size();i++){
+			if(prices[i] < low){
+				low = prices[i];
+				high = prices[i];
 			}
+
+			if(prices[i] > high){
+				high = prices[i];
+
+				if(high-low > maxgain){
+					maxgain = high-low;
+				}
+			}
+
+			r[i] = maxgain;
 		}
 		
+		return r;
+    }
+
+	vector<int> maxProfitRight(vector<int> &prices) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+		vector<int> r;
+
+		int n=prices.size();
+        int low, high, maxgain;
+		if(n == 0) return r;
+		r.resize(n);
+
+		low=high=prices[n-1];
+		maxgain = 0;
+		r[n-1] = 0;
+
+		for(int i=n-2;i>=0;i--){
+			if(prices[i] > high){
+				low = prices[i];
+				high = prices[i];
+			}
+
+			if(prices[i] < low){
+				low = prices[i];
+
+				if(high-low > maxgain){
+					maxgain = high-low;
+				}
+			}
+
+			r[i] = maxgain;
+		}
+		
+		return r;
+    }
+
+	int maxProfit(vector<int> &prices) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        int maxgain = 0;
+		int n = prices.size();
+		if(n == 0) return 0;
+
+		vector<int> left = maxProfitLeft(prices);
+		vector<int> right = maxProfitRight(prices);
+
+		for(int i=0;i<n;i++){
+			maxgain = max(maxgain, left[i] + right[i]);
+		}
+
 		return maxgain;
     }
 };
 
 void main(){
 	
-	int a[] = {1,2,1,2,3,4};
+	int a[] = {8,3,6,2,8,8,8,4,2,0,7,2,9,4,9};
 	vector<int> v(a,a+sizeof(a)/sizeof(a[0]));
 
 	Solution s;

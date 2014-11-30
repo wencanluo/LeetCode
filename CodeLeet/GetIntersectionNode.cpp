0b29fcf5d3055f4f717aa6dfbfb5751d6efa9954
wencanluo <wencanluo.cn@gmail.com>
2014-11-30 18:23:44 -0500

    add GetIntersectionNode.cpp

---
+++ CodeLeet/GetIntersectionNode.cpp
@@ -0,0 +1,32 @@
+#include<isotream>
+#include<stdlib>
+#include<cstddef>
+
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+    	return NULL;
+    }
+};
+
+int main(){
+	cout << "done!" << endl;
+	return 0;
+}

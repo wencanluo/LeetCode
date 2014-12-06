83f4c1196a53ff653f722dc69e88c2bfe9abd93b
Wencan Luo <wencanluo.cn@gmail.com>
2014-12-06 05:03:49 +0000

    solved findPeakElement

---
+++ CodeLeet/findPeakElement.cpp
@@ -0,0 +1,22 @@
+/*Find Peak Element Total Accepted: 763 Total Submissions: 2554 My Submissions Question Solution 
+A peak element is an element that is greater than its neighbors.
+
+Given an input array where num[i] ≠ num[i+1], find a peak element and return its index.
+
+You may imagine that num[-1] = num[n] = -∞.
+
+For example, in array [1, 2, 3, 1], 3 is a peak element and your function should return the index number 2.
+
+click to show spoilers.
+
+Author: Wencan Luo
+Language: C++
+Date: 12/05/2014
+*/
+
+class Solution {
+public:
+    int findPeakElement(const vector<int> &num) {
+        
+    }
+};

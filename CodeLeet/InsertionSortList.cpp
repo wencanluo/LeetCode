790f0238a96f54b3d8a3ef0fe6c92727089c02db
Wencan Luo <wencanluo.cn@gmail.com>
2015-01-07 14:45:27 +0000

    solved MaximumGap

---
+++ CodeLeet/InsertionSortList.cpp
@@ -0,0 +1,38 @@
+/*MInsertion Sort List Total Accepted: 29382 Total Submissions: 115285 My Submissions Question Solution 
+Sort a linked list using insertion sort.
+
+Author: Wencan Luo
+Language: C++
+Date: 12/07/2014
+*/
+
+#include<iostream>
+using namespace std;
+
+#include<vector>
+#include<stack>
+#include<string>
+#include<map>
+/**
+ * Definition for singly-linked list.
+ * **/
+ 
+struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode *insertionSortList(ListNode *head) {
+        
+    }
+};
+
+int main(){
+    Solution s;
+    
+
+    cout << "done";
+}
